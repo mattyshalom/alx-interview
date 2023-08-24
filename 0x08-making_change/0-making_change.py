62dc4f9e8a394f42c0d3377e2ae90e0e120135b7
mattyshalom <olusegesunday@yahoo.com>
2023-08-24 15:57:15 +0000

    making changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+ng-change mod
+"""
+
+
+def makeChange(coins, total):
+    """
+        get the smallest number of change to make from each coin
+    """
+    change = 0
+    index = 0
+    # sort coins in descending order
+    coins.sort(reverse=True)
+    while total > 0:
+        if index >= len(coins):
+            return -1
+        if coins[index] <= total:
+            total -= coins[index]
+            change += 1
+        else:
+            index += 1
+    return change
